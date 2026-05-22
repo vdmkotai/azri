fd8ff9538bf4acc79bef368420ec3e238557c922
Vadym Kotai <vdmkotai@gmail.com>
2026-05-22 21:09:08 +0400

    feat(renderer): self-contained bundler with OG meta + size check (T25)
    Signed-off-by: Vadym Kotai <vdmkotai@gmail.com>

---
+++ packages/renderer/src/bundler/index.ts
@@ -0,0 +1,4 @@
+// SPDX-License-Identifier: Apache-2.0
+// Copyright (c) 2026 Azri contributors
+
+export { bundleSelfContained, type BundleOptions, type BundleResult } from './inline.ts';
--- packages/renderer/src/index.ts
+++ packages/renderer/src/index.ts
@@ -4,5 +4,7 @@
 export * from './design-system/index.ts';
 export * from './components/index.ts';
 export * from './diagrams/index.ts';
+export * from './bundler/index.ts';
+export { renderPage, type RenderOptions } from './render.ts';
 export { escapeAttr, escapeHtml } from './utils/escape-html.ts';
 export { markdownToHtml } from './utils/markdown.ts';
